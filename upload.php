cbe24f89b475fd4b54e8efef43a51fae808c9d92
saru <saru@inf.shizuoka.ac.jp>
2015-10-19 23:53:00 +0900

    upload.php init

---
+++ upload.php
@@ -0,0 +1,26 @@
+<?php
+
+$dirname = dirname(__FILE__);
+$filename = basename($_FILES['userfile']['name']);
+sscanf($filename, "%04d%02d%02d%s", $year, $month, $day, $footer);
+//$datedir = date("Y/m/d/");
+$datedir = sprintf("/%04d/%02d/%02d/", $year, $month, $day);
+$uploaddir = $dirname . $datedir;
+
+echo $uploaddir . "\n";
+mkdir($uploaddir, 0777, TRUE);
+
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file upload attack!\n";
+}
+
+print_r($_FILES);
+
+print "</pre>";
+
+?>
